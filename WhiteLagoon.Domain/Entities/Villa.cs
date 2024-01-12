e4775fcc92ce83d3c63b96d4fe334c9cdf5c7b0e
Jithesh.Poojary <Jithesh.Poojary@TL300>
2024-01-12 16:06:55 +0530

    CRUD operation on Villa

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,21 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
-
+        [MaxLength(50)]
         public required string Name { get; set; }
 
         public string? Description { get; set; }
 
+        [Display(Name="Price Per Night")]
+        [Range(10,10000)]
         public double Price { get; set; }
 
         public int Sqft { get; set; }
 
+        [Range(1,10)]
         public int Occupancy { get; set; }
 
+        [Display(Name="Image Url")]
         public string? ImageUrl { get; set; }
 
         public DateTime? Created_Date { get; set; }
--- WhiteLagoon.web/Controllers/VillaController.cs
+++ WhiteLagoon.web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.web.Controllers
@@ -15,6 +16,74 @@ namespace WhiteLagoon.web.Controllers
         {
             var villas=_db.Villas.ToList();
             return View(villas) ;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+		public IActionResult Create(Villa villa)
+		{
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("name", "The Name and Description may not be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(villa);
+                _db.SaveChanges();
+                //return RedirectToAction("Index","Villa");     //can also be used controller as a parameter just to identify to which controller it belongs
+                return RedirectToAction("Index");
+            }
+            return View(villa);
+		}
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (villa == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(villa) ;
+        }
+
+		[HttpPost]
+		public IActionResult Update(Villa villa)
+		{
+			
+			if (ModelState.IsValid && villa.Id>0)
+			{
+				_db.Villas.Update(villa);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(villa);
+		}
+
+		public IActionResult Delete(int villaId)
+		{
+			Villa? villa = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+			if (villa is null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
+			return View(villa);
+		}
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa? objFromDb= _db.Villas.FirstOrDefault(u=>u.Id==villa.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(villa);
         }
     }
 }
